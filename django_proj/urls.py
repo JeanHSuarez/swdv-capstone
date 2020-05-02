7e59c2c76a032b5592ef00e0be9f567932ca5a90
Jean H Suarez <devyoyong@gmail.com>
2020-05-02 10:46:05 -0400

    adding staticdirs

--- django_proj/urls.py
+++ django_proj/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from persons import views as user_views
 
 urlpatterns = [
-    path('', name='login'),
+    path('', auth_views.LoginView.as_view(template_name='persons/login.html'), name='login'),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
