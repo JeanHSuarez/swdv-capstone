52aaceb74d1cd5f7b91f0587ef48240bd9bbcc1a
Jean H Suarez <devyoyong@gmail.com>
2020-07-27 20:16:15 -0400

    deleting dupliate app

--- time_keeper/timesheet/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-
-urlpatterns = [
-    path('', views.home, name='timesheet-home'),
-    path('about/', views.about, name='timesheet-about'),
-]
