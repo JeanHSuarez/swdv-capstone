3998c760196f3be29f3714aa40a89c1703094709
JeanHSuarez <devyoyong@gmail.com>
2020-07-30 22:46:57 -0400

    adding comments and testing

--- persons/tests.py
+++ persons/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from persons.models import Person
+from persons.models import Employee
 
-# Create your tests here.
+class PersonTestCase(TestCase):
+	def setUp(self):
+		Employee.objects.create(ssn="123456789", firstName="Jude")
+
+	def test_persons(self):
+		jude =  Animal.objects.get(firstName="Jude")
+		self.assertEqual(jude.ssn(), "123456789")
+
+
+# Create your tests here. 
